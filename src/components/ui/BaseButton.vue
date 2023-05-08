087c1471442d18a61eb9d29ecc2d1c96479202e7
shehab267 <shehabelhawary267@gmail.com>
2023-05-08 17:08:51 +0300

    feat: Create authentication for login and sign up
    - Integrate with Firebase backend for authentication and login with email
    - Create a basic design for login and signup form
    - Use local storage for storing token, userId, and token expiration
    - Add autologin feature with an expiration token

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,76 @@
+<template>
+  <button v-if="!link" :class="mode">
+    <slot></slot>
+  </button>
+  <router-link v-else :to="to" :class="mode">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    // mode prop used to bind class, so I can pass any class I want
+    mode: {
+      type: String,
+      requiered: false,
+      default: null,
+    },
+    // pass 'link' to render Targeted elment "RouterLink" at the template
+    link: {
+      type: Boolean,
+      requiered: false,
+      default: false,
+    },
+    // :bind to prop useful to render RouterLink to my own 'to'
+    // * !Important for 'router-link' in other components
+    to: {
+      type: String,
+      requiered: false,
+      default: '/',
+    },
+  },
+};
+</script>
+<style scoped>
+button,
+a {
+  text-decoration: none;
+  padding: 0.75rem 1.5rem;
+  font: inherit;
+  background-color: #3a0061;
+  border: 1px solid #3a0061;
+  color: white;
+  cursor: pointer;
+  border-radius: 30px;
+  margin-right: 0.5rem;
+  display: inline-block;
+}
+
+a:hover,
+a:active,
+button:hover,
+button:active {
+  background-color: #270041;
+  border-color: #270041;
+}
+
+.flat {
+  background-color: transparent;
+  color: #3a0061;
+  border: none;
+}
+
+.outline {
+  background-color: transparent;
+  border-color: #270041;
+  color: #270041;
+}
+
+.flat:hover,
+.flat:active,
+.outline:hover,
+.outline:active {
+  background-color: #edd2ff;
+}
+</style>
